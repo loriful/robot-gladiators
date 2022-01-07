df544587ed993272b7c46611cdffd2390b0b14c2
loriful <tiptonica@gmail.com>
2022-01-07 17:54:22 -0500

    Fight all robot enemies

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,9 +10,8 @@ var enemyAttack = 12;
 
 // fight function
 var fight = function(enemyName) {
-  // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
-
+  
+  while (enemyHealth > 0) {
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
@@ -58,9 +57,12 @@ var fight = function(enemyName) {
   } else {
     window.alert("You need to pick a valid option. Try again!");
   }
-};
+  }
+}
 
 // run fight function to start game
 for (var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
